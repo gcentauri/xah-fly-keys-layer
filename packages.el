eac0903e07a2b775c388510f15ff5d0befdf6615
Grant Shangreaux <shshoshin@protonmail.com>
2019-06-18 00:08:50 -0500

    Add: updates to README, including main keymap descriptions

--- packages.el
+++ packages.el
@@ -9,24 +9,6 @@
 ;;
 ;;; License: GPLv3
 
-;;; Commentary:
-
-;; See the Spacemacs documentation and FAQs for instructions on how to implement
-;; a new layer:
-;;
-;;   SPC h SPC layers RET
-;;
-;;
-;; Briefly, each package to be installed or configured by this layer should be
-;; added to `xah-fly-keys-packages'. Then, for each package PACKAGE:
-;;
-;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
-;;   function `xah-fly-keys/init-PACKAGE' to load and initialize the package.
-
-;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
-;;   define the functions `xah-fly-keys/pre-init-PACKAGE' and/or
-;;   `xah-fly-keys/post-init-PACKAGE' to customize the package as it is loaded.
-
 ;;; Code:
 
 (defconst xah-fly-keys-packages
