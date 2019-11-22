b288808663efb7cf04ce068cd9567c60de849525
Grant Shangreaux <shshoshin@protonmail.com>
2019-11-22 15:58:56 -0600

    Add: hacky use of bind-map to give xfk precedence over slime-space

--- packages.el
+++ packages.el
@@ -17,9 +17,9 @@
     helm
     ivy
     magit
+    slime
     )
   )
-
 
 (defun xah-fly-keys/init-xah-fly-keys ()
   (use-package xah-fly-keys
@@ -27,6 +27,8 @@
     :diminish (xah-fly-keys . "∑")
     :init
     (setq xah-fly-use-control-key nil)
+    (define-minor-mode xah-fly-keys-command-mode
+      "Indicates when command mode is active so `bind-map' can give leader key precedence")
 
     :config
     (xah-fly-keys-set-layout xah-fly-keys-layout)
@@ -35,8 +37,7 @@
     (add-hook 'xah-fly-insert-mode-activate-hook  'xah-fly-keys-insert-mode-on)
     (define-key xah-fly-key-map (kbd "M-SPC") 'xah-fly-command-mode-activate)
     (xah-fly-keys 1)
-    )
-  )
+    (xah-fly-keys-command-mode 1)))
 
 (defun xah-fly-keys/post-init-helm ()
   (setq-default
@@ -55,7 +56,19 @@
     (progn
       (add-hook 'magit-mode-hook 'xah-fly-insert-mode-activate)
       (add-hook 'magit-popup-mode-hook 'xah-fly-insert-mode-activate)
-      )
-    ))
+      )))
+
+(defun xah-fly-keys/post-init-slime ()
+    ;; Used to ensure the SPC leader key takes precedence over certain other minor-mode
+    ;; bindings (like several slime packages) when xah-fly-keys command mode is active
+    ;; I'm not sure if i need to require `bind-map' explicitly, as its part of the Spacemacs
+    ;; bootstrapping process?
+    (with-eval-after-load 'slime
+      (bind-map xah-fly-leader-key-map
+        :keys ("SPC")
+        :minor-modes (xah-fly-keys-command-mode)
+        :override-minor-modes t
+        :override-mode-name xah-fly-keys-override-mode)
+      (xah-fly-keys-command-mode 1)))
 
 ;; packages.el ends here
